3844f8bc8957614966535ffc4ab039c2940c6f88
Fadouse <fadouse@163.com>
2024-06-29 15:40:48 +0800

    init

---
+++ Alevel-Python/ifstatement.py
@@ -0,0 +1,13 @@
+# Hi! in this py file , i will teach you a simple if statement
+# In any if statement, its head seems like this if boolean :
+# The boolean will decide if the code will go in to if body.
+
+# like here the boolean value is false, so code won't go in to if body.
+if False:
+    print("Immpossible to see this output")
+
+# If the boolean i ture, the code will go in to if body.
+if True:
+    print("100% to see this output")
+
+# boolean have to value {True / False}
---
+++ Alevel-Python/variable.py
@@ -0,0 +1,27 @@
+
+# Hi there!, this lesson will help you get in a-level computer science
+# this python file is for teaching about python's variable.
+
+# define a variable
+# in python, you didn't need to reference a variable type
+variableInt = 1
+# above line is the define of variable, "variableInt" is the variable's name, "=" is the ways to give variable value
+print(variableInt) # output the variableInt 1
+
+# variable can change its value, but in same type;
+variableInt = 2
+print(variableInt) # output the variableInt after changed
+
+# variable can be multi types
+variableString = "Hello World"
+print(variableString)
+variableBool = True
+print(variableBool)
+variableFloat = 3.14
+print(variableFloat)
+variableDouble = 1.14514
+print(variableDouble)
+
+#Try to define a variable and output it in console
+
+
